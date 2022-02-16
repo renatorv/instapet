e92b3cac146a82145c174e72727554092b51d686
Renato Alves <renato@Mac-mini-de-Renato.local>
2022-02-15 22:37:23 -0300

    inicio da criacao do text input

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,16 +1,43 @@
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:instaped/app/componts/instaped_input.dart';
 import './login_controller.dart';
 
 class LoginPage extends GetView<LoginController> {
-    
-    static const String ROUTE_PAGE = '/login';
+  static const String ROUTE_PAGE = '/login';
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('LoginPage'),),
-            body: Container(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: Get.mediaQuery.size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/cropped-akps_header.png',
+                width: 400,
+              ),
+              SizedBox(
+                child: Column(
+                  children: [
+                    InstapedInput(
+                      label: 'E-mail',
+                    ),
+                    InstapedInput(
+                      suffixIcon: Icon(FontAwesome.key),
+                      suffixIconOnPressed: () {},
+                      label: 'Senha',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: UiConfig.appTheme,
       getPages: UiConfig.routes,
     );
